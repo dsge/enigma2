6ac91065331bdc9213f5f9ac6fc26212cf1c76d2
dsge <gerx03@gmail.com>
2019-03-15 00:02:40 +0100

    have the build script guess the unity version

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
- ~/Unity/Hub/Editor/2019.1.0b6/Editor/Unity \
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+unityVersion=`cat ./ProjectSettings/ProjectVersion.txt | grep m_EditorVersion: |  rev | cut -d: -f1 | rev | sed 's/^[[:blank:]]*//;s/[[:blank:]]*$//'`
+
+~/Unity/Hub/Editor/$unityVersion/Editor/Unity \
     -quit \
     -batchmode \
     -logfile \
     -nographics \
     "$@" \
-    -executeMethod  MyEditorBuild.PerformBuild \
-    
+    -executeMethod  MyEditorBuild.PerformBuild
+
 
