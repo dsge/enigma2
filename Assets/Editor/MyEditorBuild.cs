e37860dc7ae45babb65d39cc94e83f848fd551ac
dsge <gerx03@gmail.com>
2019-03-14 23:51:38 +0100

    add windows build

--- Assets/Editor/MyEditorBuild.cs
+++ Assets/Editor/MyEditorBuild.cs
@@ -3,14 +3,22 @@ using UnityEditor;
 // https://forum.unity.com/threads/build-script-cannot-find-menuitem.535889/#post-3531599
 class MyEditorBuild
 {
-     static void PerformBuild ()
-     {
+    static void PerformBuild () {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
         buildPlayerOptions.locationPathName = "build/linux/app";
         buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
         buildPlayerOptions.options = BuildOptions.None;
         //buildPlayerOptions.options = BuildOptions.Development | BuildOptions.AllowDebugging;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+
+    static void PerformWindowsBuild () {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "build/windows/app.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.None;
         BuildPipeline.BuildPlayer(buildPlayerOptions);
     }
 }
