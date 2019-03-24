8970359d8888e23acefc93c963512a93ea80f18d
dsge <gerx03@gmail.com>
2019-03-24 21:39:15 +0100

    - add some helpful comments - make it so that the player won't be "jumping" when he tries to move towards an elevation

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -29,33 +29,69 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        /**
+         * Was the CharacterController touching the ground during the last move?
+         */
         if (controller.isGrounded)
         {
+            /**
+             * Returns whether the given mouse button is held down.
+             */
             if (Input.GetMouseButton(0))
             {
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
                 RaycastHit hitInfo;
                 if (Physics.Raycast(ray, out hitInfo)){
+                    /**
+                     * the player clicked on a Collider (any Collider)
+                     *
+                     * save the point that he wanted to move towards
+                     */
                     moveTowards = hitInfo.point;
                 }
             }
-
             if (!nearEnough(moveTowards, transform.position)) {
+                /**
+                 * if the player is further away from the position that he wanted to move towards, then we
+                 * calculate relatively what direction that point is from the player, to try to move that way
+                 */
                 moveDirection = (moveTowards - transform.position).normalized;
             } else {
+                /**
+                 * if the player is at the point that he wanted to move towards, then we won't move relatively at all
+                 */
                 moveDirection = Vector3.zero;
             }
+            /**
+             * Transforms direction from local space to world space.
+             */
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection = moveDirection * speed;
 
+            if (moveTowards.y > transform.position.y) {
+                /**
+                 * if the player tries to move towards a position that is higher up then we don't want him to "jump" towards that higher point
+                 *
+                 * going on even ground or going down (into the ground) should not be problematic like that
+                 */
+                moveDirection.y = 0;
+            }
+
+            /**
+             * Look in the direction that we are moving...
+             */
             playerCharacter.transform.LookAt(moveTowards);
+            /**
+             * but do not look up or down
+             */
             playerCharacter.transform.eulerAngles = new Vector3(0, playerCharacter.transform.eulerAngles.y, playerCharacter.transform.eulerAngles.z);
         }
+        /**
+         * keep moving even in the air
+         */
         moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
         controller.Move(moveDirection * Time.deltaTime);
-
-
     }
 
     bool nearEnough(Vector3 target, Vector3 currentPosition) {
