6a0c19af5a7607168877e43b2b4c371227029e66
milan.adravecz <milan.adravecz@gmail.com>
2019-03-28 21:10:03 +0100

    create basic enemy movement fixes #3

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform player;
+
+    public float movementSpeed = 7.5f;
+    public float minDistance = 1;
+    public float maxDistance = 10;
+    public float actualDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player);
+
+        actualDistance = Vector3.Distance(transform.position, player.position);
+
+        if (actualDistance >= minDistance && actualDistance <= maxDistance)
+        {
+            transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        }
+    }
+}
