6b64a7039296b708c8dd509889e2c4c9d81bc6bc
dsge <gerx03@gmail.com>
2019-04-06 21:29:09 +0200

    Allow BasicMapGenerator to generate a grid of map pieces fixes #13

--- Assets/BasicMapGenerator.cs
+++ Assets/BasicMapGenerator.cs
@@ -11,7 +11,11 @@ public class BasicMapGenerator : MonoBehaviour
 
     void Start()
     {
-        this.map = generateMap(initMapParts());
+        this.map = generateMap(initMapParts(), new Vector2(5, 5));
+    }
+
+    public List<GameObject> getMap() {
+        return this.map;
     }
 
     List<GameObject> initMapParts() {
@@ -32,18 +36,22 @@ public class BasicMapGenerator : MonoBehaviour
         return ret;
     }
 
-    List<GameObject> generateMap(List<GameObject> mapPartTemplates){
+    List<GameObject> generateMap(List<GameObject> mapPartTemplates, Vector2 mapSize){
         System.Random rnd = new System.Random();
         List<GameObject> ret = new List<GameObject>();
+        /**
+         * a világ közepétől milyen távol legyen a generált pályarészek széle
+         */
+        Vector3 mapOffsetFromCenter = new Vector3(-35, 0, 0);
 
-        for (int i = 0; i < 5; i++){
-            GameObject prefab = mapPartTemplates[rnd.Next(mapPartTemplates.Count)];
-            GameObject part = Instantiate (
-                prefab,
-                new Vector3(-i * prefab.GetComponent<Renderer>().bounds.size.x - 35, 0, 0),
-                Quaternion.identity
-            );
-            ret.Add(part);
+        for (int j = 0; j < mapSize.y; j++){
+            for (int i = 0; i < mapSize.x; i++){
+                GameObject prefab = mapPartTemplates[rnd.Next(mapPartTemplates.Count)];
+                Vector3 prefabBoundsSize = prefab.GetComponent<Renderer>().bounds.size;
+                Vector3 position = new Vector3(-i * prefabBoundsSize.x, 0, -j * prefabBoundsSize.z) + mapOffsetFromCenter;
+                GameObject part = Instantiate (prefab, position, Quaternion.identity);
+                ret.Add(part);
+            }
         }
         return ret;
     }
