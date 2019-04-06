dc21a3e39f3639173cac4d4e95b0dc864b3789d0
dsge <gerx03@gmail.com>
2019-04-06 23:53:06 +0200

    add the warppad to the warppads layer

--- Assets/Layers.cs
+++ Assets/Layers.cs
@@ -7,4 +7,8 @@ class Layers {
      * bitmask for objects that are considered enemies that the player can attack
      */
     public const int ENEMIES = 1 << 10;
+    /**
+     * bitmask for objects that are considered warppads or warp points that the user can click on to switch zones/areas
+     */
+    public const int WARPPADS = 1 << 11;
 }
